4ef411d300cb600879484144a9239d3c28beb027
Brennan Duck <brennantduck@gmail.com>
2024-07-24 14:49:54 -0400

    added resume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,44 +2,36 @@ import HorizontalMenu, { NavLinkType } from "@/components/HorizontalMenu";
 import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 
 const heroLinks: NavLinkType[] = [
-  {
-    href: "/portfolio",
-    title: "check out my projects",
-  },
-  {
-    href: "/contact",
-    title: "get in touch with me",
-  },
+    {
+        href: "/portfolio",
+        title: "check out my projects",
+    },
+    {
+        href: "/contact",
+        title: "get in touch with me",
+    },
 ];
 
 export default function Home() {
-  return (
-    <main>
-      {/*
-            <div className="mx-auto flex justify-center items-center flex-col">
-                <ArrowBigUp size={50} />
-                <p className="font-mono">
-                    please go somewhere else, this page is boring
-                </p>
+    return (
+        <main>
+            <div className="hero min-h-[calc(70vh)]">
+                <div className="hero-content text-center">
+                    <div className="max-w-5xl">
+                        <h1 className="text-5xl font-mono">Hello there</h1>
+                        <p className="py-6 font-mono">
+                            My name is Brennan Duck. I am a full-stack web dev. I do
+                            JavaScript and Go mostly.
+                        </p>
+                        <div className="mx-auto flex justify-center items-center mt-16 gap-8">
+                            <ArrowBigDown size={50} />
+                            <p className="font-mono">want to learn more?</p>
+                            <ArrowBigDown size={50} />
+                        </div>
+                        <HorizontalMenu links={heroLinks} />
+                    </div>
+                </div>
             </div>
-*/}
-      <div className="hero min-h-[calc(70vh)]">
-        <div className="hero-content text-center">
-          <div className="max-w-5xl">
-            <h1 className="text-5xl font-mono">Hello there</h1>
-            <p className="py-6 font-mono">
-              My name is Brennan Duck. I am a full-stack web dev. I do
-              JavaScript and Go mostly.
-            </p>
-            <div className="mx-auto flex justify-center items-center mt-16 gap-8">
-              <ArrowBigDown size={50} />
-              <p className="font-mono">want to learn more?</p>
-              <ArrowBigDown size={50} />
-            </div>
-            <HorizontalMenu links={heroLinks} />
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
---
+++ src/app/resume/page.tsx
@@ -0,0 +1,40 @@
+import fs from "fs";
+import matter from "gray-matter";
+import {
+  Download,
+  DownloadCloud,
+  DownloadIcon,
+  FileText,
+  LucideDownload,
+} from "lucide-react";
+import { MDXRemote } from "next-mdx-remote/rsc";
+import Link from "next/link";
+import path from "path";
+
+function getThing() {
+  const mdFile = fs.readFileSync(
+    path.join("src/app/resume/resume.mdx"),
+    "utf-8",
+  );
+  const { data: frontMatter, content } = matter(mdFile);
+
+  return { frontMatter, content };
+}
+export default async function Page() {
+  const props = getThing();
+
+  return (
+    <>
+      <article className="mx-auto prose prose-sm md:prose-base lg:prose-lg prose-slate prose-i py-10 ">
+        <Link
+          href="/brennan-duck.pdf"
+          className="hover:text-red-500 transition-colors flex justify-end items-center gap-2 pr-2 no-underline"
+        >
+          <p className="m-0 text-xs font-mono">View PDF</p>
+          <FileText size={16} />
+        </Link>
+        <MDXRemote source={props.content}></MDXRemote>
+      </article>
+    </>
+  );
+}
