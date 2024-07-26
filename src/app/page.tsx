7c929b079059cd0081aa394b04e95c8af347b437
Brennan Duck <brennantduck@gmail.com>
2024-07-26 11:02:22 -0400

    misc styling fixes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,49 +3,49 @@ import { ArrowBigDown, ArrowBigUp } from "lucide-react";
 import Link from "next/link";
 
 const heroLinks: NavLinkType[] = [
-  {
-    href: "/portfolio",
-    title: "my projects",
-  },
-  {
-    href: "/contact",
-    title: "contact me",
-  },
+    {
+        href: "/portfolio",
+        title: "my projects",
+    },
+    {
+        href: "/contact",
+        title: "contact me",
+    },
 ];
 
 export default function Home() {
-  return (
-    <main>
-      <div className="hero min-h-[calc(70vh)]">
-        <div className="hero-content text-center">
-          <div className="max-w-5xl">
-            <h1 className="text-3xl lg:text-5xl font-mono">Hello there</h1>
-            <p className="py-6 font-semibold text-sm lg:text-md">
-              My name is Brennan Duck. I am a full-stack web dev. I do
-              JavaScript and Go mostly.
-            </p>
-            <div className="pt-5 lg:pt-16">
-              <p className="font-mono">want to learn more?</p>
-              <ul className="mx-auto flex justify-center items-center pt-6 gap-8">
-                {heroLinks.map((link) => (
-                  <li
-                    key={link.title}
-                    className="flex flex-col justify-center items-center"
-                  >
-                    <ArrowBigDown size={50} />
-                    <Link
-                      className="hover:text-red-500 transition-colors text-[1rem] lg:text-lg font-semibold"
-                      href={link.href}
-                    >
-                      {link.title}
-                    </Link>
-                  </li>
-                ))}
-              </ul>
+    return (
+        <main>
+            <div className="hero min-h-[calc(70vh)]">
+                <div className="hero-content text-center">
+                    <div className="max-w-5xl">
+                        <h1 className="text-3xl lg:text-5xl font-mono">Hello there</h1>
+                        <p className="py-6 font-semibold text-sm lg:text-md">
+                            My name is Brennan Duck. I am a full-stack web dev. I do
+                            JavaScript and Go mostly.
+                        </p>
+                        <div className="pt-5 lg:pt-16">
+                            <p className="font-mono">want to learn more?</p>
+                            <ul className="mx-auto flex justify-center items-center pt-6 gap-8">
+                                {heroLinks.map((link) => (
+                                    <li
+                                        key={link.title}
+                                        className=""
+                                    >
+                                        <Link
+                                            className="hover:text-red-500 text-[1rem] lg:text-lg font-semibold flex flex-col justify-center items-center"
+                                            href={link.href}
+                                        >
+                                            <ArrowBigDown size={50} />
+                                            <span>{link.title}</span>
+                                        </Link>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
-          </div>
-        </div>
-      </div>
-    </main>
-  );
+        </main>
+    );
 }
--- src/components/HorizontalMenu.tsx
+++ src/components/HorizontalMenu.tsx
@@ -1,30 +1,30 @@
 import Link from "next/link";
 
 export type NavLinkType = {
-    href: string;
-    title: string;
+  href: string;
+  title: string;
 };
 
 interface HorizontalMenuProps {
-    links: NavLinkType[];
-    className?: string;
+  links: NavLinkType[];
+  className?: string;
 }
 export default function HorizontalMenu({
-    links,
-    className,
+  links,
+  className,
 }: HorizontalMenuProps) {
-    return (
-        <ul className={"flex justify-center flex-wrap gap-3 " + className}>
-            {links.map((link) => (
-                <li key={link.title} className="">
-                    <Link
-                        className="hover:text-red-500 transition-colors text-[.5rem] lg:text-lg font-semibold"
-                        href={link.href}
-                    >
-                        {link.title}
-                    </Link>
-                </li>
-            ))}
-        </ul>
-    );
+  return (
+    <ul className={"flex justify-center flex-wrap gap-3 " + className}>
+      {links.map((link) => (
+        <li key={link.title} className="">
+          <Link
+            className="hover:text-red-500 transition-colors text-[.75rem] lg:text-lg font-semibold"
+            href={link.href}
+          >
+            {link.title}
+          </Link>
+        </li>
+      ))}
+    </ul>
+  );
 }
